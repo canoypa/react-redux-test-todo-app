d65d5fe540973e59d51f2c092501edb40e1741bf
cano <contact.sasasaky@gmail.com>
2020-04-29 11:49:57 +0900

    rollup-plugin-babel -> @rollup/plugin-babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import commonjs from "@rollup/plugin-commonjs";
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import serve from "rollup-plugin-serve";
 import { terser } from "rollup-plugin-terser";
