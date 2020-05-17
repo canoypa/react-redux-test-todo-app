b5c6e67ff13b50bf8859cacee33beb7af5896f7f
cano <contact.sasasaky@gmail.com>
2020-05-17 17:58:13 +0900

    省略

--- src/actions/todo/index.ts
+++ src/actions/todo/index.ts
@@ -2,14 +2,10 @@ export const ADD_TODO: "ADD_TODO" = "ADD_TODO";
 export const TOGGLE_COMPLETED: "TOGGLE_COMPLETED" = "TOGGLE_COMPLETED";
 export const DELETE_TODO: "DELETE_TODO" = "DELETE_TODO";
 
-export const addTodo = (text: string) => {
-  const todo = { id: Math.random(), text, complete: false };
-
-  return {
-    type: ADD_TODO,
-    data: todo,
-  };
-};
+export const addTodo = (text: string) => ({
+  type: ADD_TODO,
+  data: { id: Math.random(), text, complete: false },
+});
 
 export const toggleCompleted = (id: number, isCompleted: boolean) => ({
   type: TOGGLE_COMPLETED,
