c2f30b02b8434084e17753bf7b2cf41fd0eb4ccf
Cano <contact.sasasaky@gmail.com>
2020-07-26 23:18:13 +0900

    rollup config

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+  input: "src/index.tsx",
+
+  output: {
+    dir: "docs",
+    format: "es",
+    sourcemap: true,
+  },
+
+  plugins: [
+    babel({ extensions: [".js", ".ts", ".tsx"] }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
+
+    commonjs({ extensions: [".js", ".ts", ".tsx"] }),
+    nodeResolve(),
+
+    terser(),
+  ],
+};
